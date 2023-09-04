cf2c22a0f12440e56d7308d2b3e43139dff5c14d
Pratik-Rampuria <143866569+Pratik-Rampuria@users.noreply.github.com>
2023-09-04 21:25:42 +0530

    Add files via upload

---
+++ C77/main.js
@@ -0,0 +1,16 @@
+var fname="PRATIK";
+var lname="RAMPURIA";
+var full_name= fname.concat(lname);
+console.log(full_name)
+
+var n=Math.sqrt(2500);
+console.log(n)
+
+var x=Math.pow(4, 5);
+console.log(x)
+
+var mi= Math.min(15, 54);
+console.log(mi)
+
+var ma=Math.max(58, 19);
+console.log(ma)
